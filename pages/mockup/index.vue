58f666518e0293d68b0137d5c8db341e4c0dd4f5
qiaodamao <1@shijuefuhao.com>
2026-08-27 13:35:17 +0800

    优化移动端样式

--- pages/mockup/index.vue
+++ pages/mockup/index.vue
@@ -390,7 +390,7 @@ onMounted(() => {
     <header
       class=" h-[64px] max-h-[64px] flex backdrop-filter backdrop-blur-md sticky top-0 z-40 w-full flex-none transition-colors duration-300 lg:z-50 border-b border-gray-950/10 dark:border-gray-50/[0.2] bg-white/[0.5] dark:bg-gray-950/[0.5]">
       <!-- content -->
-      <div class="flex-1 flex items-center justify-between w-full px-8">
+      <div class="flex-1 flex items-center justify-between w-full px-5 sm:px-8">
         <!-- title -->
         <div>
           <slot name="title">
